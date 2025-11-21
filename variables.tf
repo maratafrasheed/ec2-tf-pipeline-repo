c83351d434ccce71c54335e3fcc607aae7bd2e1e
maratafrasheed <153658919+maratafrasheed@users.noreply.github.com>
2025-11-21 15:56:08 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,3 +5,15 @@ variable "region" {
 variable "instance_type" {
   default = "t2.micro"
 }
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+  default = "my-bucket-tf-test-repo"
+}
+
+variable "environment" {
+  description = "Environment tag"
+  type        = string
+  default     = "dev"
+}
